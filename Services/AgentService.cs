f1c229f2720ae6eb0917bcf1a224c25ec5b67d3a
Leopoldo Berumen Juárez <inheritech@gmail.com>
2023-12-08 20:29:10 -0600

    Saving before ditching console application

---
+++ Services/AgentService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace VrcEventAgent.Services {
+    internal class AgentService(
+        ILogger<AgentService> logger
+    ) : IHostedService {
+
+        public async Task StartAsync(CancellationToken cancellationToken) {
+            logger.LogInformation("Agent is starting...");
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken) {
+            logger.LogInformation("Agent is stopping...");
+        }
+    }
+}
